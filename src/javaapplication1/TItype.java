9070bb999e215311a4b1b3c04eb45620b3b7f011
lanilukas@gmail.com <lukas@lukas-VirtualBox.(none)>
2013-05-02 07:10:08 +0200

    First commit

---
+++ src/javaapplication1/TItype.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.*;
+/**
+ *
+ * @author lukas
+ */
+public class TItype {
+        Date time;
+        String region;
+        String name;
+        String description;
+        RType type;
+}
